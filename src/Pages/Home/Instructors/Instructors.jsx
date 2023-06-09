65bf682564450e9f2aca7bd4e4b5fe27759dd342
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-06-09 17:28:48 +0600

    intructors

--- src/Pages/Home/Instructors/Instructors.jsx
+++ src/Pages/Home/Instructors/Instructors.jsx
@@ -1,13 +1,56 @@
 import React from 'react';
 import SectionTitle from '../../../components/SectionTIitle/SectionTitle';
+import { Navigation, Pagination, Scrollbar, A11y,Autoplay } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+import img from '../../../assets/assignment-12-img/T-img/1 (1).jpg'
+import img2 from '../../../assets/assignment-12-img/T-img/2.jpg'
+import img3 from '../../../assets/assignment-12-img/T-img/3.jpg'
+import img4 from '../../../assets/assignment-12-img/T-img/4.jpg'
+
+
 
 const Instructors = () => {
+
+
     return (
         <>
             <div className='mt-12'>
                 <SectionTitle heading='Our Popular' colors='Instructors' subHeading='Join our martial art club and be healthy.' />
-                <h1>Our Instructors</h1>
             </div>
+            <Swiper
+                // install Swiper modules
+                modules={[Autoplay,Navigation, Pagination, Scrollbar, A11y]}
+                
+                centeredSlides={true}
+                autoplay={{
+                    delay: 3500,
+                    disableOnInteraction: false,
+                  }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+                className="mySwiper mx-auto"
+            >
+                <SwiperSlide>
+                    <img className='mx-auto ' src={img} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='mx-auto' src={img2} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='mx-auto' src={img3} alt="" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='mx-auto' src={img4} alt="" />
+                </SwiperSlide>
+            </Swiper>
         </>
     );
 };
